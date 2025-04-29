ed844b3de7a2b40253fce2d85844299c069f19cd
arryaanjain <jainarryaan@gmail.com>
2025-04-29 16:08:37 +0530

    Adding player apis for CRUD, setting docker for first time, creating my own docker file and building it while I also pull it, it was intense :)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlayerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/players', [PlayerController::class, 'index']);
+Route::get('/players/{player}', [PlayerController::class, 'show']);
+Route::post('/players', [PlayerController::class, 'store']);
+Route::put('/players/{player}', [PlayerController::class, 'update']);
+Route::delete('/players/{player}', [PlayerController::class, 'destroy']);
